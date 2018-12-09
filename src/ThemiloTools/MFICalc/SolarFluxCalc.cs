4a07d56a875bcf75b2829e13fbe961cc2e3bf94e
DeltaDizzy <EJBraswell@gmail.com>
2018-12-08 22:39:11 -0600

    namespaces everywhere

--- src/ThemiloTools/MFICalc/SolarFluxCalc.cs
+++ src/ThemiloTools/MFICalc/SolarFluxCalc.cs
@@ -4,36 +4,36 @@ using ModularFI;
 
 namespace ThemiloTools
 {
-    /// <summary>
-    /// This class queries the solar flux at the active veesel's position and prints the result using a ScreenMessage.
-    /// </summary>
-    [KSPAddon(KSPAddon.Startup.Flight, false)]
-    public class SolarFluxCalc : MonoBehaviour
+    namespace MFICalc
     {
         /// <summary>
-        /// These are the messages to print. One prints the value, the other says to wait until daytime.
+        /// This class queries the solar flux at the active veesel's position and prints the result using a ScreenMessage.
         /// </summary>
-        public string fluxMsg1 = "Solar Flux for ";
-        public string Is = "is ";
-        public string units = " Watts/m^2";
-        public string eclipseMsg = "You are not in direct Sunlight!  Plase wait until day to measure again.";
-        public string finalMsg;
-
-        /// <summary>
-        /// This prints the value
-        /// </summary>
-        void Update()
+        [KSPAddon(KSPAddon.Startup.Flight, false)]
+        public class SolarFluxCalc : MonoBehaviour
         {
+            public string fluxMsg1 = "Solar Flux for ";
+            public string Is = "is ";
+            public string units = " Watts/m^2";
+            public string eclipseMsg = "You are not in direct Sunlight!  Plase wait until day to measure again.";
+            public string finalMsg;
 
-            if (Input.GetKeyDown(KeyCode.Semicolon))
+            /// <summary>
+            /// This prints the value
+            /// </summary>
+            void Update()
             {
-                finalMsg = string.Format(fluxMsg1, FlightGlobals.ActiveVessel.GetCurrentOrbit().referenceBody, Is, (ModularFlightIntegrator.ActiveVesselFI.solarFlux * ModularFlightIntegrator.ActiveVesselFI.solarFluxMultiplier), units);
-                Debug.Log("[MFICALC]: Post Message!");
-                ScreenMessages.PostScreenMessage(Localizer.Format(finalMsg));
-            }
-            else if (!FlightGlobals.ActiveVessel.directSunlight)
-            {
-                ScreenMessages.PostScreenMessage(Localizer.Format(eclipseMsg));
+
+                if (Input.GetKeyDown(KeyCode.Semicolon))
+                {
+                    finalMsg = string.Format(fluxMsg1, FlightGlobals.ActiveVessel.GetCurrentOrbit().referenceBody, Is, (ModularFlightIntegrator.ActiveVesselFI.solarFlux * ModularFlightIntegrator.ActiveVesselFI.solarFluxMultiplier), units);
+                    Debug.Log("[MFICALC]: Post Message!");
+                    ScreenMessages.PostScreenMessage(Localizer.Format(finalMsg));
+                }
+                else if (!FlightGlobals.ActiveVessel.directSunlight)
+                {
+                    ScreenMessages.PostScreenMessage(Localizer.Format(eclipseMsg));
+                }
             }
         }
     }
