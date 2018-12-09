bed401ecbec94013814bb22839e67b01cb8c7df6
DeltaDizzy <EJBraswell@gmail.com>
2018-12-08 23:34:17 -0600

    panel work

---
+++ src/ThemiloTools/ThemiloTools/KopWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace ThemiloTools
+{
+    public class KopWrapper
+    {
+        static Assembly kopAssembly;
+        static Assembly kopComponentsAssembly;
+        public Type sopan;
+
+        public KopWrapper()
+        {
+            if (kopAssembly == null || kopComponentsAssembly == null)
+            {
+                foreach (AssemblyLoader.LoadedAssembly la in AssemblyLoader.loadedAssemblies)
+                {
+                    if (la.name == "Kopernicus")
+                    {
+                        kopAssembly = la.assembly;
+                    }
+                    else if (la.name == "Kopernicus.Components")
+                    {
+                        kopComponentsAssembly = la.assembly;
+                    }
+                    else
+                    {
+                        Debug.Log("[ThemiloTools] Kopernicus doesnt seem to be Installed");
+                        return;
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
---
+++ src/ThemiloTools/ThemiloTools/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThemiloTools
+{
+    public class Utils
+    {
+        public static string GameDataPath
+        {
+            get
+            {
+                return KSPUtil.ApplicationRootPath.Replace("\\", "/") + "GameData/";
+            }
+        }
+    }
+}
